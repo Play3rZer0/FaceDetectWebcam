d2ba840c73530698de06930fba8086ba8bfa7479
VinceT <37818562+Play3rZer0@users.noreply.github.com>
2020-03-09 00:34:51 +0800

    Add files via upload

---
+++ facedetect_web.py
@@ -0,0 +1,38 @@
+# Import the library modules
+import cv2
+import sys
+
+# Specify the path where Haar Cascade XML file is located
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Prepare capture from webcam
+video_capture = cv2.VideoCapture(0)
+
+while True:
+# Capture the video frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+# Drawing rectangle around the face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# Show the frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture when program terminates
+video_capture.release()
+cv2.destroyAllWindows()
